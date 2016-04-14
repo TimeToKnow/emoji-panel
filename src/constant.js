e80634b74fc6db591304c0fd9d32689760111778
Yuval Saraf <yuvalsaraf@gmail.com>
2016-04-14 11:01:59 +0300

    Forgot adding some files

---
+++ src/constant.js
@@ -0,0 +1,18 @@
+export const IMAGE_SET = {
+  APPLE: 0,
+  GOOGLE: 1,
+  TWITTER: 2,
+  EMOJIONE: 4
+};
+
+export const CATEGORIES = {
+  Activity: 'Activity',
+  Flags: 'Flags',
+  Foods: 'Foods',
+  Nature: 'Nature',
+  Objects: 'Objects',
+  People: 'People',
+  Places: 'Places',
+  Symbols: 'Symbols',
+  Other: null
+};
