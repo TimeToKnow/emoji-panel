fcefc95c0b59a22d18dce5855316c52e6d3d6333
Yuval Saraf <yuvalsaraf@gmail.com>
2016-04-14 12:15:11 +0300

    Changed some names, also recieving panel container instead of creating own

--- example/example.js
+++ example/example.js
@@ -1,5 +1,5 @@
 import EmojiPanel from 'emoji-panel';
 
 document.getElementById('button-example-1').addEventListener('click', e => {
-  new EmojiPanel();
+  new EmojiPanel(document.getElementById('panel-example-1'));
 });
--- src/emoji-panel.js
+++ src/emoji-panel.js
@@ -1,43 +1,27 @@
 import { IMAGE_SET } from './constant';
 import getImageSetTemplate from './get-image-set-template';
 
-let existingWindowImageSetsWrapper;
-const existingWindowImageSets = {};
-export default class EmojiWindow {
-  static getWindowImageSetByImageSet(imageSet) {
-    const existingWindowImageSet = existingWindowImageSets[imageSet];
-    // If windowImageSet exists on object, return it,
-    // If not, create it
-    // Only one imageSet should be present
-    return existingWindowImageSet !== undefined ? existingWindowImageSet : EmojiWindow.createWindowImageSet(imageSet);
-  }
+export default class EmojiPanel {
   static createWindowImageSet(imageSet) {
     const windowImageSet = document.createElement('div');
-    existingWindowImageSets[imageSet] = windowImageSet;
-
     const imageSetTemplate = getImageSetTemplate(imageSet);
     windowImageSet.innerHTML = imageSetTemplate;
 
-    const windowImageSetsWrapper = EmojiWindow.getWindowImageSetsWrapper();
-    windowImageSetsWrapper.appendChild(windowImageSet);
-
     return windowImageSet;
   }
-  static getWindowImageSetsWrapper() {
-    if (existingWindowImageSetsWrapper === undefined) {
-      existingWindowImageSetsWrapper = document.createElement('div');
-      document.body.appendChild(existingWindowImageSetsWrapper);
-    }
-    return existingWindowImageSetsWrapper;
-  }
-  constructor({ imageSet = IMAGE_SET.APPLE, position } = {}) {
+  constructor(el, { imageSet = IMAGE_SET.APPLE } = {}) {
     if (__DEV__) {
+      if (!(el && el.nodeType)) {
+        throw new Error('Element must be provided to the first argument of `EmojiPanel` constructor.');
+      }
       if (Object.keys(IMAGE_SET).map(key => IMAGE_SET[key]).indexOf(imageSet) === -1) {
-        throw new Error('`imageSet` should have one of `EmojiWindow.IMAGE_SET` values, got ${imageSet}.');
+        throw new Error('`imageSet` should have one of `EmojiPanel.IMAGE_SET` values, got ${imageSet}.');
       }
     }
-    const windowImageSet = EmojiWindow.getWindowImageSetByImageSet(imageSet);
+    const windowImageSet = EmojiPanel.createWindowImageSet(imageSet);
+    el.innerHTML = '';
+    el.appendChild(windowImageSet);
   }
 }
-// EmojiWindow static properties
-EmojiWindow.IMAGE_SET = IMAGE_SET;
+// EmojiPanel static properties
+EmojiPanel.IMAGE_SET = IMAGE_SET;
