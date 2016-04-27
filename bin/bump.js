e2233da15d2c36f101c690b3ffb2d441be10639f
Yuval Saraf <yuvalsaraf@gmail.com>
2016-04-27 16:31:08 +0300

    Made release pushes include bower and compiled files too

--- bin/bump.js
+++ bin/bump.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const execSync = require('child_process').execSync;
 const bump = process.argv[2] || 'patch';
 
 execSync(`npm version ${bump} --no-git-tag-version`, {
   stdio: [0, 1, 2]
 });
-execSync(`bower version ${bump}`, {
+const version = require('../package.json').version;
+
+const bowerFilePath = '../bower.json';
+const bowerFile = require(bowerFilePath);
+bowerFile.version = version;
+
+fs.writeFileSync(bowerFilePath, JSON.stringify(bowerFile, null, 2));
+
+execSync(`git add package.json bower.json && git commit -m "v${version}" && git tag v${version}`, {
   stdio: [0, 1, 2]
 });
