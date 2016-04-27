1274b5d7b5998bbfb6c9bea51bfc87693d7aa0e3
Yuval Saraf <yuvalsaraf@gmail.com>
2016-04-27 16:12:43 +0300

    Added bower, and fixed npm css peer-dep

--- bin/bump.js
+++ bin/bump.js
@@ -1,6 +1,9 @@
 const execSync = require('child_process').execSync;
 const bump = process.argv[2] || 'patch';
 
-execSync(`npm version ${bump}`, {
+execSync(`npm version ${bump} --no-git-tag-version`, {
+  stdio: [0, 1, 2]
+});
+execSync(`bower version ${bump}`, {
   stdio: [0, 1, 2]
 });
