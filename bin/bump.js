c4698c7b61f3609cb15242f0acf8ed6affb94a17
Yuval Saraf <yuvalsaraf@gmail.com>
2016-04-27 13:01:03 +0300

    Removed unused param from bump script and made it add git tag

--- bin/bump.js
+++ bin/bump.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const execSync = require('child_process').execSync;
 const bump = process.argv[2] || 'patch';
 
-execSync(`npm version --no-git-tag-version ${bump}`, {
-  stdio: [0, 1, 2],
-  cwd: path.join(__dirname, '..')
+execSync(`npm version ${bump}`, {
+  stdio: [0, 1, 2]
 });
