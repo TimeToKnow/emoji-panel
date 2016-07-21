01c024be3d5614c1d9720aeeb3c05fe9811dae6a
Yuval Saraf <yuvalsaraf@gmail.com>
2016-07-21 14:25:27 +0300

    Fix lint issues

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
+// Disabling `no-undef` because we are using here require.context which requires files dynamically, instead of regular ES6 imports
+/* eslint-disable no-undef */
+
 // load all specs into one bundle
 const testsContext = require.context('.', true, /-spec\.js$/);
 testsContext.keys().forEach(testsContext);
@@ -5,3 +8,5 @@ testsContext.keys().forEach(testsContext);
 // require all `src/**/*.js`
 const componentsContext = require.context('../src/', true, /\.js$/);
 componentsContext.keys().forEach(componentsContext);
+
+/* eslint-enable no-undef */
